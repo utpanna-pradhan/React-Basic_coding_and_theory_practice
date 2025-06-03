9f9914037be1e2d54008161e9f6c2fe10ae0a950
Utpanna Pradhan <Utpanna828@gmail.com>
2025-06-03 23:58:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quest1 from './components/Quest1'
+
+function App() {
+  return (
+    <div>
+      <Quest1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Quest1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Quest1() {
+  return (
+    <div>
+      <h1>Create a functional component called Welcome that displays “Hello, React!”.</h1>
+      <h2>Hello ,React!</h2>
+    </div>
+  )
+}
+
+export default Quest1
